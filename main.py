52c8155d95cd766cb03b61875a79c3617d9f563d
Ifeanyi Otuonye <ifeanyiro@gmail.com>
2023-05-20 20:56:29 -0400

    Created Route tables and Routes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import boto3
 import vpc_subnets
 import internet_nat_gateway
+import route_tables
 
 # Create the VPC
 response_vpc = vpc_subnets.create_custom_vpc('10.0.0.0/16','Name', '3Tier-VPC')
@@ -44,12 +45,13 @@ print("Create Private Subnet 3 Response")
 print(response_priv_sub1)
 print("")
 
-# Create private subnet 4
+#Create private subnet 4
 response_priv_sub4 = vpc_subnets.create_custom_subnet("us-east-1b", "10.0.178.0/20", vpc_id, "Name", "3Tier-Priv-Sub4")
 print("Create Private Subnet 4 Response")
 print(response_priv_sub4)
 print("")
 
+#Create and attach Internet Gateway
 response_create_attach_ig = internet_nat_gateway.create_attach_internet_gateway(vpc_id, "Name", "3Tier-Int-GW")
 print("Create and attach Iternet Gateway Response")
 print(response_create_attach_ig)
@@ -61,8 +63,33 @@ print("Get Subnet ID for NAT Gateway")
 print(subnet_id_ng)
 print("")
 
+#Create NAT Gateway
 response_create_ng = internet_nat_gateway.create_nat_gateway(subnet_id_ng, "Name", "3Tier-EIP", "Name", "3Tier-Nat-GW")
 print("Create NAT Gateway Response")
+print(response_create_ng)
+print("")
+
+#Get Internet Gateway ID
+int_gw_id = response_create_attach_ig["InternetGateway"]["InternetGatewayId"]
+print("Get Internet Gateway ID")
+print(int_gw_id)
+print("")
+
+#Create Route table and Route to Internet Gateway
+response_public_rt = route_tables.create_pub_rt(vpc_id, "0.0.0.0/0", int_gw_id, "Name", "3Tier-Pub-RT")
+print("Create Route Table for Internet Gateway Response")
+print(response_create_ng)
+print("")
+
+#Get NAT Gateway ID
+nat_gw_id = response_create_attach_ig["NatGateway"]["NatGatewatId"]
+print("Get NAT Gateway ID")
+print(nat_gw_id)
+print("")
+
+#Create Route table and Route to NAT Gateway
+response_private_rt = route_tables.create_priv_rt(vpc_id, "0.0.0.0/0", nat_gw_id, "Name", "3Tier-Priv-RT")
+print("Create Route Table for NAT Gateway Response")
 print(response_create_ng)
 print("")
 
--- route_tables.py
+++ route_tables.py
@@ -1,1 +1,70 @@
 import boto3
+
+ec2 = boto3.client('ec2')
+
+def create_pub_rt(vpc_id, dest_cidr, int_gw_id, tags_key, tags_value):
+    #create public routetable
+    response_rt = ec2.create_route_table(
+        VpcId=vpc_id,
+        TagSpecifications=[
+            {
+                'ResourceType': 'route-table',
+                'Tags': [
+                    {
+                        'Key': tags_key,
+                        'Value': tags_value
+                    },
+                ]
+            },
+        ]
+    )
+    
+    rt_id = response_rt["RouteTable"]["RouteTableId"]
+
+    #Create route for routetable to internet gateway
+    response_route = ec2.create_route(
+        DestinationCidrBlock=dest_cidr,
+        GatewayId=int_gw_id,
+        RouteTableId=rt_id,
+    )
+
+    print("Create Private Route to Natgateway Responce")
+    print(response_route)
+    print("")
+
+    return response_rt
+
+
+
+
+def create_priv_rt(vpc_id, dest_cidr, nat_gw_id, tags_key, tags_value):
+    #create private routetable
+    response_rt = ec2.create_route_table(
+        VpcId=vpc_id,
+        TagSpecifications=[
+            {
+                'ResourceType': 'route-table',
+                'Tags': [
+                    {
+                        'Key': tags_key,
+                        'Value': tags_value
+                    },
+                ]
+            },
+        ]
+    )
+
+    rt_id = response_rt["RouteTable"]["RouteTableId"]
+   
+    #Create route for routetable to natgateway
+    response_route = ec2.create_route(
+        DestinationCidrBlock=dest_cidr,
+        NatGatewayId=nat_gw_id,
+        RouteTableId=rt_id,
+    )
+
+    print("Create Private Route to Natgateway Responce")
+    print(response_route)
+    print("")
+
+    return response_rt
